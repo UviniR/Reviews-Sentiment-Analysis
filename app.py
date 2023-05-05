440f152b5febfc15111c8b016191538f0c2bef56
Uvini Ranaweera <87543333+UviniR@users.noreply.github.com>
2023-05-05 14:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if file is not None:
 #                   .set_table_attributes('style="position: sticky; top: 0;"')
 #                   .to_html(index=False, escape=False))
     
-    table_html = df.style.set_table_styles([{ 'selector': 'table',    'props': [('border', '0'), ('border-collapse', 'collapse')]
+    table_html = df.style.set_table_styles([{ 'selector': 'table',    'props': [('border', 'none'), ('border-collapse', 'collapse')]
 }]).render()
 
     
